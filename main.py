b0910e5f0dad098cff38001f9c56ed650c77d784
SaacUnal <sealarcon@unal.edu.co>
2025-02-22 00:32:01 +0000

    Update

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from restaurant.menu import *
 from restaurant.order import Order
 from restaurant.payment import *
 from restaurant.queue import Queue
+from collections import namedtuple
 
 if __name__ == "__main__":
   # Elements - this could be a db
-  nonAlcoholicBeverage = NonAlcoholicBeverage("Gazimba", 3000, ["cocaina", "azucar"], 500)
-  appetizer = Appetizer("Salchichon", 2500, ["perro", "sal"])
-  mainCourse = MainCourse("Carne", 50000, ["gato", "sal", "pimienta recien molida"])
-  veganMainCourse = VeganMainCourse("Hamburguesa", 100000000, ["garbanzo", "quimicos"])
-  cocktail = Cocktail("tequila", 45000, ["alcohol", "diversion :)"], 600)
-  pastries = Pastries("galleta", 3000, ["harina", "azucar", "otras cosas"])
-  dessert = Dessert("banana split", 7500,["banana", "split", "helado"])
-  fruitSalad = FruitSalad("Ensalada", 15000, ["frutas", "queso"])
-  fastFood = FastFood("Pizza", 3000, ["PIÑA", "queso"])
-  kidsMenu = KidsMenu("nuggets", 3000, ["pollo", "microplasticos"])
+  Ingredientes = namedtuple("Ingredientes",["Solido", "Liquido", "Polvo", "Otro"], defaults=(None,) * 4) 
+  nonAlcoholicBeverage = NonAlcoholicBeverage("Gazimba", 3000, Ingredientes( Polvo=["cocaina", "azucar"]), 500)
+  appetizer = Appetizer("Salchichon", 2500, Ingredientes(Solido="perro", Polvo="sal"))
+  mainCourse = MainCourse("Carne", 50000, Ingredientes(Solido="gato", Polvo=["sal", "pimienta recien molida"]))
+  veganMainCourse = VeganMainCourse("Hamburguesa", 100000000, Ingredientes(Solido="garbanzo", Otro="quimicos"))
+  cocktail = Cocktail("tequila", 45000, Ingredientes(Liquido="alcohol", Otro="diversion :)"), 600)
+  pastries = Pastries("galleta", 3000, Ingredientes(Polvo=["harina", "azucar"], Otro="otras cosas"))
+  dessert = Dessert("banana split", 7500,Ingredientes(Solido=["banana", "helado"], Otro="split"))
+  fruitSalad = FruitSalad("Ensalada", 15000, Ingredientes(["frutas", "queso"]))
+  fastFood = FastFood("Pizza", 3000, Ingredientes(["PIÑA", "queso"]))
+  kidsMenu = KidsMenu("nuggets", 3000, Ingredientes(Solido="pollo", Otro="microplasticos"))
 
   # Orders - this will be used by the waiter
   # For the sake of the customer you gotta use a FIFO 
